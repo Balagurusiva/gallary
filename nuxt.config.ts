7f9a76d64b075fefbdd380825e4082b676c0a043
Balaguru <balagurusiva2002@gmail.com>
2024-05-31 11:13:22 +0530

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: ["@nuxt/image",'@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-})
+	ssr: false,
+	routeRules: {
+		"/": { isr: 600 },
+	},
+	devtools: { enabled: false },
+	modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+	css: ["~/assets/css/main.css"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+});
