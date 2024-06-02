00d9c39234fa054e017f22ededd61931a4660652
Balaguru <balagurusiva2002@gmail.com>
2024-06-02 19:19:31 +0530

    modified:   assets/css/main.css   	modified:   pages/dubai-International.vue   	modified:   pages/dubai-fashion-runway.vue   	modified:   pages/dubai-international-fashion.vue   	modified:   pages/index.vue   	modified:   pages/product-launch-taj-chennai.vue

--- pages/dubai-fashion-runway.vue
+++ pages/dubai-fashion-runway.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        dubai fashion
+    <div class="w-full h-full flex flex-col items-center bg-[#f9f3ff]">
+		<!-- header section -->
+		<header
+			class="header sticky top-0 flex pt-[5px] flex-col w-full items-center bg-[#f9f3ff]"
+		>
+			<div>
+				<p
+					class="text-[18px] sm:text-[32px] xl:text-[32px] font-extrabold font-serif text-[#7F5E9F]"
+				>
+					Moments Captured in Time
+				</p>
+			</div>
+			<div class="logo">
+				<NuxtImg src="/Kx1rLCnLKU4d4cD7viBNCJUbSbQpkU3HIpEeP4U0.png" />
+			</div>
+		</header>
     </div>
 </template>
