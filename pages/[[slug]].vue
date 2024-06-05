a1557c6195f2f227b0b7369e887951bb6a2f4552
Balaguru <balagurusiva2002@gmail.com>
2024-06-05 10:06:51 +0530

    modified:   pages/[[slug]].vue

--- pages/[[slug]].vue
+++ pages/[[slug]].vue
@@ -39,11 +39,24 @@
 <script setup> 
 import taj_imgs from '~/public/product-launch-taj-chennai';
 	const route = useRoute();
-	const imgs = ref();
+	const imgs = ref([
+        "/product-launch-taj-chennai/img1.jpg",
+    "/product-launch-taj-chennai/img2.jpg",
+    "/product-launch-taj-chennai/img3.jpg",
+    "/product-launch-taj-chennai/img4.jpg",
+    "/product-launch-taj-chennai/img5.jpg",
+    "/product-launch-taj-chennai/img6.jpg",
+    "/product-launch-taj-chennai/img7.jpg",
+    "/product-launch-taj-chennai/img8.jpg",
+    "/product-launch-taj-chennai/img9.jpg",
+    "/product-launch-taj-chennai/img10.jpg",
+    "/product-launch-taj-chennai/img11.jpg",
+    "/product-launch-taj-chennai/img12.jpg",
+    ]);
 
-	if (route.params.slug == "product-launch-taj-chennai") {
-		imgs.value = taj_imgs;
-	}
+	// if (route.params.slug == "product-launch-taj-chennai") {
+	// 	imgs.value = taj_imgs;
+	// }
 </script>
 
 <style scoped>
