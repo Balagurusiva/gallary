85a8aa20793faeb6f6ff8e6bb398b2f9f74edb59
Balaguru <balagurusiva2002@gmail.com>
2024-06-05 10:12:47 +0530

    modified:   pages/[[slug]].vue 	renamed:    public/product-launch-taj-chennai/index.js -> public/product-launch-taj-chennai/taj_img.js

--- pages/[[slug]].vue
+++ pages/[[slug]].vue
@@ -37,26 +37,13 @@
 </template>
 
 <script setup> 
-import taj_imgs from '~/public/product-launch-taj-chennai';
+import taj_imgs from '~/public/product-launch-taj-chennai/taj_img';
 	const route = useRoute();
-	const imgs =[
-        "/product-launch-taj-chennai/img1.jpg",
-    "/product-launch-taj-chennai/img2.jpg",
-    "/product-launch-taj-chennai/img3.jpg",
-    "/product-launch-taj-chennai/img4.jpg",
-    "/product-launch-taj-chennai/img5.jpg",
-    "/product-launch-taj-chennai/img6.jpg",
-    "/product-launch-taj-chennai/img7.jpg",
-    "/product-launch-taj-chennai/img8.jpg",
-    "/product-launch-taj-chennai/img9.jpg",
-    "/product-launch-taj-chennai/img10.jpg",
-    "/product-launch-taj-chennai/img11.jpg",
-    "/product-launch-taj-chennai/img12.jpg",
-    ];
+	const imgs = ref();
 
-	// if (route.params.slug == "product-launch-taj-chennai") {
-	// 	imgs.value = taj_imgs;
-	// }
+	if (route.params.slug == "product-launch-taj-chennai") {
+		imgs.value = taj_imgs;
+	}
 </script>
 
 <style scoped>
